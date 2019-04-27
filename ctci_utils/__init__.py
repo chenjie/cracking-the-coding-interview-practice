3484f19760b5c49177b4137f92efe27dbf87a97f
jellycsc <nichenjie2013@gmail.com>
2019-04-27 17:24:30 -0400

    Add singly linkedlist support

---
+++ ctci_utils/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+from .linkedlist import LinkedListNode
+from .linkedlist import LinkedList
---
+++ ctci_utils/linkedlist.py
@@ -0,0 +1,24 @@
+class LinkedListNode:
+	# Standard singly linkedlist node
+
+	def __init__(self, val):
+		self.val = val
+		self.next = None
+
+	def __str__(self):
+		return str(self.val)
+
+
+class LinkedList:
+	# Wrapper for LinkedListNode
+
+	def __init__(self):
+		self.head = None
+
+	def __str__(self):
+		cur = self.head
+		val_list = []
+		while cur is not None:
+			val_list.append(str(cur.val))
+			cur = cur.next
+		return 'None' if len(val_list) == 0 else ' -> '.join(val_list) + ' -> None'
