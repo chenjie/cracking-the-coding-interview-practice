8bf56fe4b33a50a5a73622f2ccbd32e5de97428f
jellycsc <nichenjie2013@gmail.com>
2019-04-27 21:12:01 -0400

    finish ll p1

--- ctci_utils/linkedlist.py
+++ ctci_utils/linkedlist.py
@@ -8,12 +8,26 @@ class LinkedListNode:
 	def __str__(self):
 		return str(self.val)
 
+	def print_full_ll(self):
+		cur = self
+		val_list = []
+		while cur is not None:
+			val_list.append(str(cur.val))
+			cur = cur.next
+		res = ' -> '.join(val_list) + ' -> None'
+		print(res)
+
+	def add_to_front(self, node):
+		node.next = self
+		return node
+
 
 class LinkedList:
 	# Wrapper for LinkedListNode
 
 	def __init__(self):
-		self.head = None
+		print("Not very useful. DO NOT USE!")
+		# self.head = None
 
 	def __str__(self):
 		cur = self.head
